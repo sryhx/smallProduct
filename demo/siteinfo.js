58fa5982e38e2a619e5862086ba939f51a0841b7
Your Name <you@example.com>
2021-08-05 11:42:35 +0800

    11 commit

--- demo/siteinfo.js
+++ demo/siteinfo.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "小程序名称",
   version: "1.0.0",
   siteroot: "https://xxx.com",
-  api_key:"jlaskdjlkas12312",
+  api_key:"xxx",
   api_secret:"填写小程序秘钥",
   design_method: "3"
 };
